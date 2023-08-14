d1861a1694bff77c347b89bde7bfcca1cebdb9a0
IShidlovskiy <shdlvsk-ivan@yandex.ru>
2023-08-14 18:44:25 +0300

    еще меньше строк

--- 1 sorting.py
+++ 1 sorting.py
@@ -4,9 +4,7 @@ test_data = [9, 5, 8, 1, 2]
 def sort(data: list):
     for k in range(len(data) - 1):
         for i in range(len(data)-1-k):
-            if data[i] <= data[i+1]:
-                continue
-            else:
+            if data[i] > data[i+1]:
                 data[i], data[i+1] = data[i+1], data[i]
     return data
 
