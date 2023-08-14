01246f8602ca3dc3adcbcfe8be65e66a1aee8385
IShidlovskiy <shdlvsk-ivan@yandex.ru>
2023-08-14 21:17:34 +0300

    решение

--- 6_prime_number.py
+++ 6_prime_number.py
@@ -2,22 +2,12 @@ numbers = [i for i in range(2, 1000)]
 
 
 def only_prime(nums: list) -> list:
-    cut_list = []
-    for i in range(len(nums)):
-        if nums[i] == 1:
-            continue
-        if nums[i] != 2 and nums[i] % 2 == 0:
-            continue
-        if nums[i] != 3 and nums[i] % 3 == 0:
-            continue
-        prime = True
-        for n in range(2, nums[i]):
-            if nums[i] % n == 0:
-                prime = False
-                break
-        if prime:
-            cut_list.append(nums[i])
-    return cut_list
+    primes = []
+    while nums:
+        prime = nums[0]
+        primes.append(prime)
+        nums = [num for num in nums if num % prime != 0]
+    return primes
 
 
 print(only_prime(numbers))
